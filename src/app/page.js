3eb89d6d2ce5ad67868efef3665697c196b356d2
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-10-25 15:13:16 +0530

    adding two things

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
+'use client'
+import { useState } from "react";
 import styles from "./page.module.css";
-
 export default function Home() {
+  const [name, setName] = useState("Ritik")
+  const apple = () => {
+    setName("Pradeep")
+  }
+  const InnerCompo= () => {
+    return (
+      <h1>Innner componenet</h1>
+    )
+  }
   return (
     <div className={styles.page}>
-     <main className={styles.main}>
-       <h1>JAI SHREE KRISHNA</h1>
+      <main className={styles.main}>
+        <h1>Events, function and state {name}</h1>
+        <button onClick={()=>apple("Fruit")}>Click me</button>
+        <InnerCompo />
+        {InnerCompo()}
       </main>
     </div>
   );
