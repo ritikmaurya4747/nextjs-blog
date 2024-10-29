d02d8df395a3bf0f6d506d2f18143e0df10adfa7
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-10-29 17:07:19 +0530

    adding mudular css to do not overwrite css based on same class name

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from "react"
-
+import styles from '../page.module.css'
 export default function Page(){
     const [style,setStyle]= useState({backgroundColor:"green"})
     return (
@@ -9,6 +9,7 @@ export default function Page(){
             <h4>This is red Text</h4>
             <h3 style={{backgroundColor:"blue"}}>This is red Text</h3>
             <h2 style={style}>Heading 3 for user</h2>
+            <h2 className={styles.module}>This is mudular css</h2>
             <button onClick={()=> setStyle({backgroundColor:"yellow"})}>Update Style</button>
         </div>
     )
