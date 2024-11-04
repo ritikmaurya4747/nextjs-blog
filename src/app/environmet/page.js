63675e107d6aeb6525227ec2489f788e15b30d12
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-11-04 17:27:51 +0530

    add env file

---
+++ src/app/environmet/page.js
@@ -0,0 +1,15 @@
+export default function Page(){
+    console.log(process.env.SERVER_PASSWORD);
+    
+    return (
+        <div>
+        {
+            process.env.NODE_ENV=="development"?
+            <h1>You are on development Mode</h1>
+            :
+            <h1>You are on Production Mode</h1>
+        }
+            <h1>Environment Variables in next js </h1>
+        </div>
+    )
+}
