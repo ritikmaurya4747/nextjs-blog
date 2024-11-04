a5bfc79a57db8120029211f39cfc014cbb24d14d
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-11-04 17:35:48 +0530

    adding config file

--- src/app/environmet/page.js
+++ src/app/environmet/page.js
@@ -1,15 +1,17 @@
-export default function Page(){
-    console.log(process.env.SERVER_PASSWORD);
-    
-    return (
-        <div>
-        {
-            process.env.NODE_ENV=="development"?
-            <h1>You are on development Mode</h1>
-            :
-            <h1>You are on Production Mode</h1>
-        }
-            <h1>Environment Variables in next js </h1>
-        </div>
-    )
+import { API_BASE_URL } from "../config/constants";
+
+export default function Page() {
+  console.log(process.env.SERVER_PASSWORD);
+
+  return (
+    <div>
+      {process.env.NODE_ENV == "development" ? (
+        <h1>You are on development Mode</h1>
+      ) : (
+        <h1>You are on Production Mode</h1>
+      )}
+      <h1>Environment Variables in next js </h1>
+      {API_BASE_URL}
+    </div>
+  );
 }
